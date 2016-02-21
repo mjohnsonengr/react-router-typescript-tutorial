d349d2e0457480bb1d649f212dd31f1766deb700
Marcus Johnson <marcusj@windward.net>
2016-02-20 22:11:12 -0700

    Figured out how best to specify the props interface for App.

--- modules/App.tsx
+++ modules/App.tsx
@@ -2,26 +2,27 @@ import * as React from 'react';
 import {Link} from 'react-router';
 import {NavLink} from './NavLink';
 
-interface AppProps {
-    children: React.ReactElement<{}>;
-}
+interface AppProps extends React.Props<App> { }
 
-export function App(props:AppProps) {
-    return (
-        <div>
-            <h1>Ghettohub Issues</h1>
-            <ul role="nav">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/repos">Repos</NavLink>
-                </li>
-            </ul>
-            {props.children}
-        </div>
-    );
+export class App extends React.Component<AppProps, {}> {
+    public render()
+    {
+        return (
+            <div>
+                <h1>Ghettohub Issues</h1>
+                <ul role="nav">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/repos">Repos</NavLink>
+                    </li>
+                </ul>
+                {this.props.children}
+            </div>
+        );
+    }
 }
